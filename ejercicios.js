e5f6a266bebb953054cd5c1e19d4587f84badea2
jglg2205 <josegregoriolopez22@gmail.com>
2023-07-24 23:26:10 -0500

    primer commit con ejemplos de variables

---
+++ ejercicios.js
@@ -0,0 +1,5 @@
+var miNombre = "jose";
+//la funcion console.log sirve para mostrar resultados en la consola del browser
+console.log(miNombre);
+var miApellido = "lopez"
+console.log(miNombre + " " + miApellido);
