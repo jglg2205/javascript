b69e35929cdab2f16718b3482be3b22d95a39ae3
jglg2205 <josegregoriolopez22@gmail.com>
2023-08-09 23:05:17 -0500

    Ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -1,5 +1,81 @@
-var miNombre = "jose";
-//la funcion console.log sirve para mostrar resultados en la consola del browser
-console.log(miNombre);
-var miApellido = "lopez"
-console.log(miNombre + " " + miApellido);
+/*var cola = [1,2,3,4,5];
+
+function miCola (arreglo, elemento){
+    var agregado = arreglo.push(elemento);
+    var removido = arreglo.shift();
+    console.log(arreglo);
+    return ("se ha agregado correctamente " + elemento + " y se ha removido de la lista: " + removido);
+
+}
+
+console.log(miCola(cola, 6));*/
+
+//Funcion para calcular  puntajes em el golf
+/*
+function calculoGolf(par, golpes){
+    if(golpes === 1){
+        return "hole in one";
+    }else if(golpes == par - 2){
+        return "eagle";
+    }else if(golpes == par - 1){
+        return  "birdie";
+    }else if(golpes == par){
+        return "par";
+    }else if(golpes == par + 1 ){
+        return "bogey";
+    }else if(golpes == par + 2){
+        return "Double bogey";
+    }else {
+        return ("go home");
+    }
+}
+
+console.log(calculoGolf(12, 14));
+*/
+
+//funcion simuladora del juego blackjak:
+
+var conteo = 0;
+
+function conteoCartas(carta){
+    var decision;
+    switch(carta){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            conteo++;
+            break;
+        
+        case 7:
+        case 8:
+        case 9:
+            conteo = conteo + 0;
+            break;
+
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            conteo--;
+            break;
+        default:
+            console.log("error de juego");
+            break;
+       
+    }
+
+    if(conteo <=0){
+        return(conteo + " Esperar ");
+    }else{
+        return(conteo + " Apostar");
+    }
+
+    
+}
+
+console.log(conteoCartas(5));
+console.log(conteoCartas("J"));
+console.log(conteoCartas("A"));
